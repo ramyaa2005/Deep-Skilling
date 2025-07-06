54a76ff0e60a2a197fa4b4874b78b1f45eb74716
ramyaa2005 <125334938+ramyaa2005@users.noreply.github.com>
2025-07-06 17:08:29 +0530

    Add files via upload

---
+++ week_3/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.spring_boot_demo;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Welcome to Spring Boot!";
+    }
+}

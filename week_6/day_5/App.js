4fbbdc8ed5f3f1bb59fc36ca137364e8e5a0a537
ramyaa2005 <125334938+ramyaa2005@users.noreply.github.com>
2025-07-26 23:33:55 +0530

    Add files via upload

--- week_6/day_5/App.js
+++ week_6/day_5/App.js
@@ -1,1 +1,28 @@
+import React, { useState, useEffect } from 'react';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
 
+function App() {
+  const [users, setUsers] = useState([]);
+
+  // Fetch users from JSONPlaceholder API
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users?_limit=5')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+
+  const addUser = (user) => {
+    setUsers([...users, { ...user, id: users.length + 1 }]);
+  };
+
+  return (
+    <div style={{ padding: '20px', fontFamily: 'Arial' }}>
+      <h1>User Management App</h1>
+      <UserForm onAddUser={addUser} />
+      <UserList users={users} />
+    </div>
+  );
+}
+
+export default App;

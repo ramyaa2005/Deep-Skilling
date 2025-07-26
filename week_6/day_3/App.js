7de45b4ed16ffd2b750f9b90f5277a2879d8af22
ramyaa2005 <125334938+ramyaa2005@users.noreply.github.com>
2025-07-26 23:16:20 +0530

    Add files via upload

--- week_6/day_3/App.js
+++ week_6/day_3/App.js
@@ -1,1 +1,32 @@
+import React, { useState } from 'react';
+import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
 
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { text, completed: false }]);
+  };
+
+  const toggleTodo = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoForm onAdd={addTodo} />
+      <TodoList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
+    </div>
+  );
+}
+
+export default App;
